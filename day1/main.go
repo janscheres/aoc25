708b73814efdf568891828e120784a4ab1142bb5
Jan Scheres <jscheresm@gmail.com>
2025-12-01 16:46:37 +0000

    Day 1 Part 2

--- day1/main.go
+++ day1/main.go
@@ -27,7 +27,40 @@ func main() {
 			numzeros++
 		}
 	}
-	fmt.Println(numzeros)
+	fmt.Println("Part 1:", numzeros)
+
+	current = 50
+	numzeros = 0
+
+	for l := range strings.Lines(input) {
+		l = strings.TrimSpace(l)
+		if l == "" {
+			continue
+		}
+
+		i, _ := strconv.Atoi(l[1:])
+
+		for a:=0; a<i; a++ {
+			if l[0] == 'R' {
+				current++
+
+				if current > 99 {
+					current = 0
+				}
+			} else {
+				current --
+
+				if current < 0 {
+					current = 99
+				}
+			}
+
+			if current==0 {
+				numzeros++
+			}
+		}
+	}
+	fmt.Println("Part 2:", numzeros)
 }
 
 /*const input = `L1
